1ae31e287d1d96d5f2ec276d633f2bca567faf96
Saeed Alizadeh <saeed.alizadeh@pipedrive.com>
2020-06-06 11:41:47 +0300

    add jest config

---
+++ backend/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  bail: true,
+  rootDir: '.',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'tsx',
+    'json',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: [
+    '<rootDir>/tests/**/*.test.(ts|js)',
+  ],
+  testEnvironment: 'node',
+};
