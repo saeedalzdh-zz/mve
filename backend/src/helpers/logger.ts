b6403915842e3ecb1e5bddd1bb40df7f7e513124
Saeed Alizadeh <saeed.alizadeh@pipedrive.com>
2020-06-06 11:44:45 +0300

    error handling logger and temporary data persistent

---
+++ backend/src/helpers/logger.ts
@@ -0,0 +1,9 @@
+// tslint:disable: no-console
+const logger = {
+	log: (message: any) => console.log(message),
+	info: (message: any) => console.info(message),
+	warn: (message: any) => console.warn(message),
+	error: (message: any) => console.error(message),
+};
+
+export default logger;
