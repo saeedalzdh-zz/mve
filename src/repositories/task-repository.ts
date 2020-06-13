2d8fece681fda7d9f8ed59e3ff419c7967104e26
Saeed Alizadeh <saeed.alizadeh@pipedrive.com>
2020-06-13 15:39:56 +0300

    update task repository by adding db

--- src/repositories/task-repository.ts
+++ src/repositories/task-repository.ts
@@ -1,39 +1,24 @@
-import { v4 as uuidv4 } from 'uuid';
 import { Task } from '../interfaces';
 
-// could replace this part later with DB and add model layer or ORM
-import { readData, createData, updateData, deleteData } from '../helpers/persist-data';
-import responseCodes from '../response-codes';
-import { errorResponse } from '../helpers/error-response';
-
 const readTasks = async (): Promise<Task[]> => {
-    return await readData().catch(errorResponse(responseCodes.READ_TASK_FAILED_IN_MODEL));
+    return [
+        {
+            id: 123,
+            title: 'harchi'
+        }
+    ]
 };
 
-const createTask = async (newTask: string): Promise<Task> => {
-    const newTaskObject = {
-        id: uuidv4(),
-        title: newTask,
-    };
-
-    await createData(newTaskObject).catch(errorResponse(responseCodes.CREATE_TASK_FAILED_IN_MODEL));
-
-    return newTaskObject;
+const createTask = async (newTask: string): Promise<void> => {
+   
 };
 
-const updateTask = async (taskId: string, newTitle: string): Promise<Task> => {
-    const updatedTaskObject = {
-        id: taskId,
-        title: newTitle,
-    };
-
-    await updateData(updatedTaskObject).catch(errorResponse(responseCodes.UPDATE_TASK_FAILED_IN_MODEL));
-
-    return updatedTaskObject;
+const updateTask = async (taskId: string, newTitle: string): Promise<void> => {
+    
 };
 
 const deleteTask = async (taskId: string): Promise<void> => {
-    await deleteData(taskId).catch(errorResponse(responseCodes.DELETE_TASK_FAILED_IN_MODEL));
+    
 };
 
 export { readTasks, createTask, updateTask, deleteTask };
